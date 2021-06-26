8c1f0fe554b84a61ec5c31e041746fbefe3dfd7a
AmolSamota <samota.amol.21apr@gmail.com>
2021-06-27 00:21:15 +0530

    first mail sent

--- src/components/CreateEmail/index.js
+++ src/components/CreateEmail/index.js
@@ -1,87 +1,119 @@
 import { useState } from "react";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import styles from "./index.module.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const Inbox = () => {
-    const [show, setShow] = useState(false);
+const CreateEmail = (props) => {
+    // const [show, setShow] = useState(props.show);
     const [to, setRecepient] = useState("");
     const [cc, setCC] = useState("");
     const [subject, setSubject] = useState("");
     const [content, setContent] = useState("");
 
+    const { token } = useSelector((state) => state.auth);
+
+    const handler = () => {
+        console.log(to, content, subject);
+        axios
+            .post(
+                process.env.REACT_APP_API_LINK + "/send-email",
+                {
+                    receivers: to,
+                    body: content,
+                    subject: subject,
+                },
+                { headers: { Authorization: `Bearer ${token}` } }
+            )
+            .then((res) => {
+                alert("email sent");
+            })
+            .catch((err) => console.log(err));
+
+        props.setShow(false);
+    };
+
     const handleClose = () => {
-        setShow(false);
+        props.setShow(false);
     };
     return (
         <Container>
-            <Modal show={show} onHide={handleClose}>
+            <Modal
+                scrollable
+                size="lg"
+                show={props.show}
+                onHide={handleClose}
+                dialogClassName={`${styles.dialogwm}`}
+            >
                 <Modal.Header closeButton>
                     <Modal.Title>Create email</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Row>
-                        <Col md={2}>
+                        <Col md={2} xs={2}>
                             <label htmlFor="to">To</label>
                         </Col>
-                        <Col>
+                        <Col md={10} xs={10}>
                             <input
                                 id="to"
                                 type="email"
                                 placeholder=""
                                 required
                                 value={to}
+                                style={{ width: "100%" }}
                                 onChange={(e) => setRecepient(e.target.value)}
                             ></input>
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={2}>
+                        <Col md={2} xs={2}>
                             <label htmlFor="CC">CC</label>
                         </Col>
-                        <Col>
+                        <Col md={10} xs={10}>
                             <input
                                 id="CC"
-                                type="text"
+                                type="email"
                                 placeholder=""
+                                style={{ width: "100%" }}
                                 value={cc}
                                 onChange={(e) => setCC(e.target.value)}
                             ></input>
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={2}>
+                        <Col md={2} xs={2}>
                             <label htmlFor="subject">Subject</label>
                         </Col>
-                        <Col md={10}>
+                        <Col md={10} xs={10}>
                             <input
                                 id="subject"
-                                rows="8"
-                                type="text"
+                                required
+                                style={{ width: "100%" }}
                                 placeholder=""
-                                value={subject}
+                                value={content}
                                 onChange={(e) => setSubject(e.target.value)}
                             ></input>
                         </Col>
                     </Row>
                     <Row>
-                        <Col>
-                            <input
+                        <Col md={2} xs={2}>
+                            <label htmlFor="content">Content</label>
+                        </Col>
+                        <Col md={10} xs={10}>
+                            <textarea
                                 id="content"
                                 rows="8"
-                                type="text"
                                 placeholder=""
+                                style={{ width: "100%" }}
                                 value={content}
                                 onChange={(e) => setContent(e.target.value)}
-                            ></input>
+                            ></textarea>
                         </Col>
                     </Row>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                    <Button variant="primary" onClick={handler}>
+                        Send
                     </Button>
                 </Modal.Footer>
             </Modal>
@@ -89,4 +121,4 @@ const Inbox = () => {
     );
 };
 
-export default Inbox;
+export default CreateEmail;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 import { Helmet } from "react-helmet";
-import Inbox from "../components/Inbox/index.js";
+import CreateEmail from "../components/CreateEmail/index.js";
 
 const HomePage = () => {
+    const handleChange = () => {
+        setShow(true);
+    };
+
+    const [show, setShow] = useState(false);
     return (
         <>
             <Helmet>
                 <title>Home Page</title>
             </Helmet>
-            {/* <Inbox /> */}
+            <CreateEmail show={show} setShow={setShow} />
+            <Row>
+                <Col className="text-center">
+                    <Button
+                        size="lg"
+                        className={`m-2 px-2 font-weight-bolder text-center`}
+                        variant="warning"
+                        onClick={() => handleChange(true)}
+                    >
+                        Create email
+                    </Button>
+                </Col>
+            </Row>
         </>
     );
 };
